4e70bafb8a10d0ed9e8d235f67a48a5c1e3bba0d
ljy <liangjy881@naver.com>
2023-04-12 22:52:08 +0900

    결과파일 저장

--- auto_prj/auto_submit_extended.py
+++ auto_prj/auto_submit_extended.py
@@ -1,6 +1,6 @@
+import re
 import pyautogui
 import time
-import pyperclip
 from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver.common.by import By
@@ -21,6 +21,14 @@ if (len(notepad) > 0):
     notepad_window.activate()  # notepad 활성화
     time.sleep(0.3)
     notepad_window.maximize()  # notepad 최대화
+    print(notepad_window.title)
+    filename_regex = re.compile(r'PB\d{8}.txt')
+    mo = filename_regex.search(notepad_window.title)
+    if mo != None:
+        filename = mo.group()
+        print(filename)
+    else:
+        exit()
 
     pyautogui.hotkey('ctrl', 'a')  # 텍스트 전체 선택
     pyautogui.hotkey('ctrl', 'c')  # 텍스트 복사
@@ -31,7 +39,6 @@ if (len(notepad) > 0):
     chr_options.add_experimental_option("detach", True)
     browser = webdriver.Chrome(
         './chromedriver.exe', options=chr_options)
-    # browser = webdriver.Chrome()
     browser.get(SITE_URL)
 
     browser.maximize_window()
@@ -52,17 +59,29 @@ if (len(notepad) > 0):
         inner_html = elem.get_attribute('innerHTML')
         print(inner_html)
         soup = BeautifulSoup(inner_html, 'html.parser')
-        # print(soup.prettify())
-        pyperclip.copy(inner_html)
+        rows = soup.select('tr.resultB')
+        result_text = ''
+        for row in rows:
+            td = row.td
+            count = len(td.contents)
+            if count > 0:
+                item = td.contents[0].string.text.strip().replace(
+                    '\n      ', '')
+                result_text += item + '\n'
+            if count > 2:
+                link = td.contents[2].string.text
+                result_text += link + '\n'
+            if count > 4:
+                bottom_id = td.contents[4].string.text
+                result_text += bottom_id + '\n'
+            result_text += '\n'
         time.sleep(0.5)
-        pyautogui.hotkey('win', 'r')
-        time.sleep(0.5)
-        pyautogui.write('notepad')
-        pyautogui.write(['enter'])
-        time.sleep(0.5)
-        pyautogui.hotkey('ctrl', 'v')
-        time.sleep(600)
+        with open(f'result_files/{filename}', 'w', encoding='utf-8') as fh:
+            fh.write(result_text)
+    except:
+        pyautogui.alert('오류 발생')
     finally:
+        pyautogui.alert('처리 완료')
         browser.quit()
 else:
     pyautogui.alert('Notepad++프로그램을 열어두세요!')
