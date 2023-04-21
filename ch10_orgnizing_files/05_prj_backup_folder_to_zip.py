b5d446e95e5661bfebd4474d4e01b4dea4cf64f0
ljy <liangjy881@gmail.com>
2023-04-21 23:55:18 +0900

    ch10 finished

---
+++ ch10_orgnizing_files/05_prj_backup_folder_to_zip.py
@@ -0,0 +1,40 @@
+"""
+Copy an entire folder and its contents into a ZIP file whose filename increments.
+"""
+
+import zipfile
+import os
+
+
+def backupToZip(folder):
+    folder = os.path.abspath(folder)  # make sure folder is absolute
+
+    # Figure out the filename this code should use based on what files already exist.
+    number = 1
+    while True:
+        zipFilename = f'{os.path.basename(folder)}_{str(number)}.zip'
+        if not os.path.exists(zipFilename):
+            break
+        number += 1
+
+    print(f'Creating {zipFilename}...')
+    backupZip = zipfile.ZipFile(zipFilename, 'w')
+
+    # Walk the entire folder tree and compress the files in each folder.
+    for foldername, _, filenames in os.walk(folder):
+        print(f'Adding files in {foldername}...')
+        # Add the current folder to the ZIP file.
+        backupZip.write(foldername)
+
+        # Add all the files in this folder to the ZIP file.
+        for filename in filenames:
+            newBase = os.path.basename(folder)+'_'
+            if filename.startswith(newBase) and filename.endswith('.zip'):
+                continue  # don't backup the backup ZIP files
+            backupZip.write(os.path.join(foldername, filename))
+    backupZip.close()
+    print('Done')
+
+
+backupToZip(
+    r'data\ch10\prj_backup_folder_to_zip')
